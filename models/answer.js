b3d84ba2917cf24925fd480da657e5b66f7d93d3
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-07-22 20:14:22 +0700

    feat: Add Answer model for storing user responses

---
+++ models/answer.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+    },
+    formId: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+    },
+
+    createdAt: {
+      type: Number,
+    },
+    updatedAt: {
+      type: Number,
+    },
+  },
+
+  {
+    timestamps: {
+      currentTime: () => Math.floor(Date.now() / 1000),
+    },
+    strict: false,
+  }
+);
+
+Schema.plugin(mongoosePaginate);
+
+export default mongoose.model("Answer", Schema);
